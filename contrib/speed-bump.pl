528c655b45d234067c556d926b6cdcd3186800a2
Alex Schroeder <alex@gnu.org>
2020-12-25 15:15:38 +0100

    Refactor speed-bump
    Using a more reasonable data structure: a single hash using IP as key,
    unlike the previous solution that grow over time.

--- contrib/speed-bump.pl
+++ contrib/speed-bump.pl
@@ -42,11 +42,11 @@ be changed in the config file.
 our $speed_bump_requests = 30;
 our $speed_bump_window = 60;
 
-# $speed_data->{visitors}->{$ip} = [$last, ... , $oldest]
-# $speed_data->{warnings}->{$ip} = [1, ... , 0]
-# $speed_data->{blocks}->{$ip}->{seconds} = $sec
-# $speed_data->{blocks}->{$ip}->{until} = $ts
-# $speed_data->{blocks}->{$ip}->{probation} = $ts + $sec
+# $speed_data->{$ip}->{visits} = [$last, ... , $oldest]
+# $speed_data->{$ip}->{warnings} = [1, ... , 0]
+# $speed_data->{$ip}->{seconds} = $sec
+# $speed_data->{$ip}->{until} = $ts
+# $speed_data->{$ip}->{probation} = $ts + $sec
 my $speed_data;
 
 # order is important: we must be able to reset the stats for tests
@@ -55,53 +55,45 @@ push(@extensions, \&speed_bump_admin, \&speed_bump);
 sub speed_bump {
   my ($stream, $url) = @_;
   my $now = time;
-  # go through all the blocks we kept and delete the old data
-  for my $ip (keys %{$speed_data->{blocks}}) {
-    # delete the time limits if they are in the past
-    if ($speed_data->{blocks}->{$ip}->{until} and $speed_data->{blocks}->{$ip}->{until} < $now) {
-      delete($speed_data->{blocks}->{$ip}->{until});
-    }
-    # if the probation period elapsed, delete all the info
-    if ($speed_data->{blocks}->{$ip}->{probation} and $speed_data->{blocks}->{$ip}->{probation} < $now) {
-      delete($speed_data->{blocks}->{$ip});
-    }
-  }
-  # go through all the request time stamps and delete data outside the time window
-  for my $ip (keys %{$speed_data->{visitors}}) {
-    # if the latest visit was longer ago than the time window, forget it
-    if (not $speed_data->{visitors}->{$ip}
-	or $speed_data->{visitors}->{$ip}->[0] + $speed_bump_window < $now) {
-      delete($speed_data->{visitors}->{$ip});
-      delete($speed_data->{warnings}->{$ip});
+  # go through the data we keep and delete it if the two time limits ellapsed
+  # and the last visit is past the time window we're interested in
+  for my $ip (keys %$speed_data) {
+    if ((not $speed_data->{$ip}->{until}
+	 or $speed_data->{$ip}->{until} < $now)
+	and (not $speed_data->{$ip}->{probation}
+	     or $speed_data->{$ip}->{probation} < $now)
+	and (not $speed_data->{$ip}->{visits}
+	     or $speed_data->{$ip}->{visits}->[0] < $now - $speed_bump_window)) {
+      delete($speed_data->{$ip});
     }
   }
-  # check if we are currently blocked now that maintenance is done
+  # check if we are currently blocked
   my $ip = $stream->handle->peerhost;
-  if (exists $speed_data->{blocks}->{$ip}) {
-    my $until = $speed_data->{blocks}->{$ip}->{until};
+  if (exists $speed_data->{$ip}) {
+    my $until = $speed_data->{$ip}->{until};
     if ($until and $until > $now) {
-      $log->debug("Extending a block");
-      my $seconds = $speed_data->{blocks}->{$ip}->{seconds};
-      $speed_data->{blocks}->{$ip}->{probation} += 2 * $seconds;
-      $speed_data->{blocks}->{$ip}->{until} += $seconds;
-      $until = $speed_data->{blocks}->{$ip}->{until};
-      my $delta = $until - $now;
+      my $seconds = speed_bump_add($ip, $now);
+      $log->debug("Extending the block by $seconds");
+      my $delta = $speed_data->{$ip}->{until} - $now;
       $stream->write("44 $delta\r\n");
       # no more processing
       return 1;
     }
   }
   # add a timestamp to the front for the current $ip
-  unshift(@{$speed_data->{visitors}->{$ip}}, $now);
+  unshift(@{$speed_data->{$ip}->{visits}}, $now);
   # add a warning to the front for the current $ip if the current URL could be a bot
-  unshift(@{$speed_data->{warnings}->{$ip}}, scalar $url =~ m!/(raw|html|diff|history|do/(?:comment|do/(?:all/(?:latest/)?)?changes/|rss|(?:all)?atom|new|more|match|search|index|tag))/!);
+  unshift(@{$speed_data->{$ip}->{warnings}},
+	  scalar $url =~ m!/(raw|html|diff|history|do/(?:comment|do/(?:all/(?:latest/)?)?changes/|rss|(?:all)?atom|new|more|match|search|index|tag))/!);
   # if there are enough timestamps, pop the last one and see if it falls within
-  # the time window
-  if (@{$speed_data->{visitors}->{$ip}} > $speed_bump_requests) {
-    pop(@{$speed_data->{warnings}->{$ip}}); # ignore: we just want the same number in both arrays
-    my $oldest = pop(@{$speed_data->{visitors}->{$ip}});
+  # the time window; if so, all the requests happened within the time window
+  # we're watching
+  if (@{$speed_data->{$ip}->{visits}} > $speed_bump_requests) {
+    pop(@{$speed_data->{$ip}->{warnings}});
+    my $oldest = pop(@{$speed_data->{$ip}->{visits}});
     if ($now < $oldest + $speed_bump_window) {
       my $seconds = speed_bump_add($ip, $now);
+      $log->debug("Block for $seconds because of too many requests");
       $stream->write("44 $seconds\r\n");
       # no more processing
       return 1;
@@ -109,9 +101,10 @@ sub speed_bump {
   }
   # even if the browsing speed is ok, we want to block you if you're visiting a
   # lot of URLs that a human would not
-  my $warnings = sum(@{$speed_data->{warnings}->{$ip}}) || 0;
+  my $warnings = sum(@{$speed_data->{$ip}->{warnings}}) || 0;
   if ($warnings > $speed_bump_requests / 3) {
     my $seconds = speed_bump_add($ip, $now);
+    $log->debug("Block for $seconds because of too many suspicious requests");
     $stream->write("44 $seconds\r\n");
     # no more processing
     return 1;
@@ -123,16 +116,18 @@ sub speed_bump {
 sub speed_bump_add {
   my $ip = shift;
   my $now = shift;
-  $log->debug("Adding peer block");
   # if so, we're going to block you, and if you're a repeating offender, we're
   # going to double the block
-  my $probation = $speed_data->{blocks}->{$ip}->{probation};
-  my $seconds = $speed_data->{blocks}->{$ip}->{seconds};
+  my $probation = $speed_data->{$ip}->{probation};
+  # the default block time is 60s for first time offenders
+  my $seconds = $speed_data->{$ip}->{seconds} || 60;
+  # if this happened within the probation time, double the block time
   $seconds *= 2 if $seconds and $probation and $probation > $now;
-  $seconds ||= 60; # the default for first time offenders
-  $speed_data->{blocks}->{$ip}->{seconds} = $seconds;
-  $speed_data->{blocks}->{$ip}->{until} = $now + $seconds;
-  $speed_data->{blocks}->{$ip}->{probation} = $now + 2 * $seconds;
+  # protect against integer overflows, haha: 1y is 365*24*60*60 = 31536000
+  $seconds = 31536000 if $seconds > 31536000;
+  $speed_data->{$ip}->{seconds} = $seconds;
+  $speed_data->{$ip}->{until} = $now + $seconds;
+  $speed_data->{$ip}->{probation} = $now + 2 * $seconds;
   return $seconds;
 }
 
@@ -182,36 +177,20 @@ sub with_speed_bump_fingerprint {
 sub speed_bump_status {
   my $stream = shift;
   $stream->write("# Speed Bump Status\n");
-  # get a list of unique IPs
-  my %ips;
-  for my $item (qw(visitors warnings blocks)) {
-    for my $ip (keys %{$speed_data->{$item}}) {
-      $ips{$ip} = 1;
-    }
-  }
   my $now = time;
   $stream->write("```\n");
   #               <-4s> <-4s> <2/2> <-4s> <-4s>    <-4s>
   $stream->write(" From    To Warns Block Until Probation IP\n");
-  for my $ip (keys %ips) {
+  for my $ip (keys %$speed_data) {
+    $log->debug($ip);
     $stream->write(sprintf("%s %s %2d/%2d %s %s     %s $ip\n",
-			   $speed_data->{visitors}->{$ip}
-			   ? speed_bump_time($speed_data->{visitors}->{$ip}->[-1] - $now)
-			   : " n/a ",
-			   $speed_data->{visitors}->{$ip}
-			   ? speed_bump_time($speed_data->{visitors}->{$ip}->[0] - $now)
-			   : " n/a ",
-			   sum(@{$speed_data->{warnings}->{$ip}}) || 0,
-			   scalar(@{$speed_data->{warnings}->{$ip}}) || 0,
-			   $speed_data->{blocks}->{$ip}
-			   ? speed_bump_time($speed_data->{blocks}->{$ip}->{seconds})
-			   : " n/a ",
-			   $speed_data->{blocks}->{$ip} && $speed_data->{blocks}->{$ip}->{until}
-			   ? speed_bump_time($speed_data->{blocks}->{$ip}->{until} - $now)
-			   : " n/a ",
-			   $speed_data->{blocks}->{$ip} && $speed_data->{blocks}->{$ip}->{probation}
-			   ? speed_bump_time($speed_data->{blocks}->{$ip}->{probation} - $now)
-			   : " n/a "));
+			   speed_bump_time($speed_data->{$ip}->{visits}->[-1], $now),
+			   speed_bump_time($speed_data->{$ip}->{visits}->[0], $now),
+			   sum(@{$speed_data->{$ip}->{warnings}}) || 0,
+			   scalar(@{$speed_data->{$ip}->{warnings}}) || 0,
+			   speed_bump_time($speed_data->{$ip}->{seconds}),
+			   speed_bump_time($speed_data->{$ip}->{until}, $now),
+			   speed_bump_time($speed_data->{$ip}->{probation}, $now)));
     # use Net::Whois::IP qw(whoisip_query); my $response = whoisip_query($ip); $response->{OrgName};
   }
   $stream->write("```\n");
@@ -221,6 +200,10 @@ sub speed_bump_status {
 
 sub speed_bump_time {
   my $seconds = shift;
+  my $now = shift;
+  return "  n/a" unless $seconds;
+  $seconds -= $now if $now;
+  return sprintf("%4dd", int($seconds/86400)) if $seconds > 172800; # 2d
   return sprintf("%4dh", int($seconds/3600)) if $seconds > 7200; # 2h
   return sprintf("%4dm", int($seconds/60)) if $seconds > 120; # 2min
   return sprintf("%4ds", $seconds);

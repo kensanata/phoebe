8ba6a895f5f4559d16ec73f1ed7afce98aac8b9c
Alex Schroeder <alex@gnu.org>
2021-06-09 00:45:55 +0200

    Add Iapetus plugin

---
+++ contrib/iapetus.pl
@@ -0,0 +1,173 @@
+# -*- mode: perl -*-
+# Copyright (C) 2021  Alex Schroeder <alex@gnu.org>
+
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <https://www.gnu.org/licenses/>.
+
+=head1 Iapetus Support
+
+This allows known editors to upload files and pages. You need to set
+C<@known_fingerprints> in your config file. Here's an example:
+
+    our (@known_fingerprints);
+    @known_fingerprints = qw(
+      sha256$fce75346ccbcf0da647e887271c3d3666ef8c7b181f2a3b22e976ddc8fa38401
+      sha256$54c0b95dd56aebac1432a3665107d3aec0d4e28fef905020ed6762db49e84ee1);
+
+The way to do it is to request the I<certificate> from your friends (not their
+key!) and run the following:
+
+    openssl x509 -in client-cert.pem -noout -sha256 -fingerprint \
+    | sed -e 's/://g' -e 's/SHA256 Fingerprint=/sha256$/' \
+    | tr [:upper:] [:lower:]
+
+This should give you your friend's fingerprint in the correct format to add to
+the list above.
+
+Make sure your main menu has a link to the login page. The login page allows
+people to pick the right certificate without interrupting their uploads.
+
+    => /login Login
+
+This code works by handling C<iapetus:> links. See
+L<https://codeberg.org/oppenlab/iapetus> for more.
+
+=cut
+
+package App::Phoebe;
+use Modern::Perl;
+use File::MimeInfo qw(globs);
+our (@extensions, @request_handlers, $log, $server, @known_fingerprints);
+push(@{$server->{wiki_mime_type}},'text/gemini');
+unshift(@request_handlers, '^iapetus://' => \&handle_iapetus);
+
+sub handle_iapetus {
+  my $stream = shift;
+  my $data = shift;
+  # extra processing of the request if we didn't do that, yet
+  return setup_iapetus($stream, $data) unless $data->{upload};
+  my $size = $data->{upload}->{params}->{size};
+  my $actual = length($data->{buffer});
+  if ($actual == $size) {
+    $log->debug("Handle Iapetus request as Titan request");
+    process_titan($stream, $data->{request}, $data->{upload}, $data->{buffer}, $size);
+    # do not close in case we're waiting for the lock
+    return;
+  } elsif ($actual > $size) {
+    $log->debug("Received more than the promised $size bytes");
+    $stream->write("59 Received more than the promised $size bytes\r\n");
+    $stream->close_gracefully();
+    return;
+  }
+  $log->debug("Waiting for " . ($size - $actual) . " more bytes");
+}
+
+sub setup_iapetus {
+  my $stream = shift;
+  my $data = shift;
+  my $request = $data->{request};
+  $log->info("Looking at $request");
+  my $hosts = host_regex();
+  my $spaces_regex = space_regex();
+  my $port = port($stream);
+  if ($request =~ m!^iapetus://($hosts)(?::$port)?!) {
+    my $host = $1;
+    my($scheme, $authority, $path, $query, $fragment, $size) =
+	$request =~ m|(?:([^:/?#]+):)?(?://([^/?#]*))?([^?#]*)(?:\?([^#]*))?(?:#(\S*))?\s+(\d+)|;
+    if ($path =~ m!^(?:/($spaces_regex))?(?:/raw)?/([^/;=&]+)!) {
+      my ($space, $id) = ($1, $2);
+      return unless valid_id($stream, $host, $space, $id);
+      my $type = globs($id) || mime_type($id);
+      my $params = { size => $size, mime => $type };
+      return unless valid_mime_type($stream, $host, $space, $id, $params);
+      return unless valid_size($stream, $host, $space, $id, $params);
+      return unless valid_client_cert($stream, $host, $space, $id, $params);
+      $data->{upload} = {
+	host => $host,
+	space => space($stream, $host, $space),
+	id => decode_utf8(uri_unescape($id)),
+	params => $params,
+      };
+      $stream->write("10 Continue\r\n"); # weird!
+      return 1;
+    } else {
+      $log->debug("The path $path is malformed");
+      $stream->write("59 The path $path is malformed\r\n");
+      $stream->close_gracefully();
+    }
+  }
+  return 0;
+}
+
+# fallback if File::MimeInfo found no data files
+sub mime_type {
+  $_ = shift;
+  return 'text/gemini' if /\.gmi$/i;
+  return 'text/plain' if /\.te?xt$/i;
+  return 'text/markdown' if /\.md$/i;
+  return 'text/html' if /\.html?$/i;
+  return 'image/png' if /\.png$/i;
+  return 'image/jpeg' if /\.jpe?g$/i;
+  return 'image/gif' if /\.gif$/i;
+  return 'text/plain'; # this is what phoebe expects
+}
+
+# duplicates functionality from registered_editor_login.pl
+
+sub valid_client_cert {
+  my $stream = shift;
+  my $host = shift;
+  my $space = shift;
+  my $id = shift;
+  my $fingerprint = $stream->handle->get_fingerprint();
+  if ($fingerprint and grep { $_ eq $fingerprint} @known_fingerprints) {
+    $log->info("Successfully identified client certificate");
+    return 1;
+  } elsif ($fingerprint) {
+    $log->info("Unknown client certificate $fingerprint");
+    $stream->write("61 Your client certificate is not authorized for editing\r\n");
+  } else {
+    $log->info("Requested client certificate");
+    $stream->write("60 You need a client certificate to edit this wiki\r\n");
+  }
+  $stream->close_gracefully();
+  return;
+}
+
+# also duplicates functionality from registered_editor_login.pl
+
+push(@extensions, \&iapetus_login);
+
+sub iapetus_login {
+  my $stream = shift;
+  my $url = shift;
+  my $hosts = host_regex();
+  my $spaces = space_regex();
+  my $port = port($stream);
+  my $fingerprint = $stream->handle->get_fingerprint();
+  my $host;
+  if (($host) = $url =~ m!^gemini://($hosts)(?::$port)?/login!) {
+    if ($fingerprint and grep { $_ eq $fingerprint} @known_fingerprints) {
+      $log->info("Successfully identified client certificate");
+      $stream->write("30 gemini://$host:$port/\r\n");
+    } elsif ($fingerprint) {
+      $log->info("Unknown client certificate $fingerprint");
+      $stream->write("61 Your client certificate is not known\r\n");
+    } else {
+      $log->info("Requested client certificate");
+      $stream->write("60 You need a client certificate to edit this wiki\r\n");
+    }
+    return 1;
+  }
+  return;
+}
---
+++ t/iapetus.t
@@ -0,0 +1,79 @@
+# Copyright (C) 2021  Alex Schroeder <alex@gnu.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use Modern::Perl;
+use Test::More;
+use File::Slurper qw(write_text read_binary);
+use IO::Socket::IP;
+use IO::Socket::SSL;
+use Mojo::IOLoop::Server;
+use utf8; # tests contain UTF-8 characters and it matters
+
+my $msg;
+if (not $ENV{TEST_AUTHOR}) {
+  $msg = 'Contributions are author test. Set $ENV{TEST_AUTHOR} to a true value to run.';
+}
+plan skip_all => $msg if $msg;
+
+our $host;
+our $base;
+our $dir;
+our $port;
+our @config = qw(iapetus.pl);
+# make sure fingerprints are known (this reuses the server certificate!)
+push(@config, <<"EOF");
+\@fingerprints = qw(sha256$0ba6ba61da1385890f611439590f2f0758760708d1375859b2184dcd8f855a00);
+EOF
+require './t/test.pl';
+
+# test page
+my $page = query_gemini("$base/Haiku");
+like($page, qr/^51 Path not found/m, "Test page does not exist");
+
+# upload text
+sub iapetus {
+  my $request = shift;
+  my $data = shift;
+  my $socket = IO::Socket::SSL->new(
+    PeerHost => $host, PeerPort => $port,
+    # don't verify the server certificate
+    SSL_verify_mode => SSL_VERIFY_NONE,
+    SSL_cert_file => 't/cert.pem',
+    SSL_key_file => 't/key.pem', );
+  $socket->print($request);
+  is(<$socket>, "10 Continue\r\n");
+  $socket->print($data);
+  undef $/; # slurp
+  return <$socket>;
+}
+
+my $haiku = <<EOT;
+Quiet keyboard tapping,
+Tests are missing, and it's late,
+My partner fast asleep.
+EOT
+
+$page = iapetus("iapetus://$host:$port/Haiku 82\r\n", $haiku);
+like($page, qr/^30 $base\/page\/Haiku\r$/, "Iapetus Haiku");
+
+$page = query_gemini("$base/page/Haiku");
+like($page, qr/^20 text\/gemini; charset=UTF-8\r\n# Haiku\n$haiku/, "Haiku saved");
+
+# plain text
+
+$page = query_gemini("$base\/raw\/Haiku");
+like($page, qr/$haiku/m, "Raw text");
+
+done_testing();

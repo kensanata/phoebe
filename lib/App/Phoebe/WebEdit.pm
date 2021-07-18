0a8b9d9eddc3a90c8125a510c7b1f1c6ade4cd58
Alex Schroeder <alex@gnu.org>
2021-07-18 14:23:06 +0200

    Fix WebEdit issue
    - import read_text

--- lib/App/Phoebe/WebEdit.pm
+++ lib/App/Phoebe/WebEdit.pm
@@ -19,7 +19,7 @@ use App::Phoebe qw(@footer @extensions @request_handlers @main_menu $server $log
 		   port space host_regex space_regex handle_http_header text
 		   with_lock wiki_dir bogus_hash http_error to_url quote_html);
 use Encode qw(decode_utf8 encode_utf8);
-use File::Slurper qw(write_text);
+use File::Slurper qw(read_text write_text);
 use Modern::Perl;
 use URI::Escape;
 
